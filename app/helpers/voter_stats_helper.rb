50e7298e23b97c77f31c6b895d9ac12b23406e41
Hannah Squier <hannahsquier@gmail.com>
2016-08-27 18:25:11 -0700

    add bootstrap theme

--- app/helpers/voter_stats_helper.rb
+++ app/helpers/voter_stats_helper.rb
@@ -1,2 +1,14 @@
 module VoterStatsHelper
+  def channel_mapper(channel)
+    channels = {
+
+    "Twitter" => link_to("<i class='fa fa-twitter fa-lg'>".html_safe, "https://www.twitter.com/#{channel['id']}"),
+
+    "YouTube" => link_to("<i class='fa fa-youtube fa-lg'>".html_safe, "https://www.youtube.com/#{channel['id']}"),
+
+    "Facebook" => link_to("<i class='fa fa-facebook fa-lg'>".html_safe, "https://www.facebook.com/#{channel['id']}")
+    }
+
+    channels[channel["type"]]
+  end
 end
