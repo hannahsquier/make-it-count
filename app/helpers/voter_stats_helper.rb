6575995c4af4e22902c256edcd4cd2070f9f1d8f
Hannah Squier <hannahsquier@gmail.com>
2016-08-27 21:20:14 -0700

    add senator info

--- app/helpers/voter_stats_helper.rb
+++ app/helpers/voter_stats_helper.rb
@@ -2,11 +2,11 @@ module VoterStatsHelper
   def channel_mapper(channel)
     channels = {
 
-    "Twitter" => link_to("<i class='fa fa-twitter fa-lg'>".html_safe, "https://www.twitter.com/#{channel['id']}"),
+    "Twitter" => link_to("<i class='fa fa-3x fa-twitter text-white sr-icons'></i>".html_safe, "https://www.twitter.com/#{channel['id']}"),
 
-    "YouTube" => link_to("<i class='fa fa-youtube fa-lg'>".html_safe, "https://www.youtube.com/#{channel['id']}"),
+    "YouTube" => link_to("<i class='fa fa-3x fa-youtube text-white sr-icons'></i>".html_safe, "https://www.youtube.com/#{channel['id']}"),
 
-    "Facebook" => link_to("<i class='fa fa-facebook fa-lg'>".html_safe, "https://www.facebook.com/#{channel['id']}")
+    "Facebook" => link_to("<i class='fa fa-3x fa-facebook text-white sr-icons'></i>".html_safe, "https://www.facebook.com/#{channel['id']}")
     }
 
     channels[channel["type"]]
